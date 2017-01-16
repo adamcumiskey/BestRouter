2dff5e32d6b702fdbb51dbcc556876e6331966b5
Adam Cumiskey <adam.cumiskey@gmail.com>
2017-01-15 23:45:43 -0500

    routers and a demo

---
+++ BestRouter/Classes/BestRouter.swift
@@ -0,0 +1,106 @@
+//
+//  BestRouter.swift
+//  Pods
+//
+//  Created by Adam Cumiskey on 1/15/17.
+//
+//
+
+import UIKit
+
+
+public protocol RouterType: class {
+    var viewController: UIViewController { get }
+}
+
+
+/// Generic Router base class
+public class Router<ViewController: UIViewController>: RouterType {
+    fileprivate var _viewController: ViewController
+    public var viewController: UIViewController { return _viewController }
+    
+    public init(viewController: ViewController) {
+        _viewController = viewController
+    }
+}
+
+
+/// Base of the Router heirarchy in a window
+/// You should create and retain this in your App Delegate when `applicationDidFinishLaunching` is called
+public final class WindowRouter<Router: RouterType>: RouterType {
+    public var root: Router
+    public var viewController: UIViewController { return root.viewController }
+    
+    private var window: UIWindow
+    
+    public init(root: Router, window: UIWindow) {
+        root.viewController.view.frame = window.bounds
+        self.root = root
+        self.window = window
+    }
+    
+    public func launch() {
+        window.rootViewController = root.viewController
+        window.makeKeyAndVisible()
+    }
+}
+
+
+/// Router wrapping UINavigationController
+public class NavigationRouter<Root: RouterType>: Router<UINavigationController> {
+    public var root: Root
+    private var delegate: UINavigationControllerDelegate?
+    
+    public init(root: Root, delegate: UINavigationControllerDelegate? = nil) {
+        self.root = root
+        
+        let navigationController = UINavigationController(rootViewController: root.viewController)
+        navigationController.delegate = delegate
+        super.init(viewController: navigationController)
+    }
+    
+    func pushRouter(router: RouterType, animated: Bool = true) {
+        self._viewController.pushViewController(router.viewController, animated: animated)
+    }
+    
+    func popRouter(animated: Bool = true) {
+        self._viewController.popViewController(animated: animated)
+    }
+}
+
+
+/// Router wrapping UITabBarController
+public class TabBarRouter: Router<UITabBarController> {
+    public var items: [RouterType]
+    private var delegate: UITabBarControllerDelegate?
+    
+    public init(items: [RouterType], delegate: UITabBarControllerDelegate? = nil) {
+        self.items = items
+        self.delegate = delegate
+        
+        let tabBar = UITabBarController()
+        tabBar.viewControllers = self.items.map { $0.viewController }
+        tabBar.delegate = delegate
+        super.init(viewController: tabBar)
+    }
+}
+
+
+/// Router wrapping UISplitViewController
+public class SplitViewRouter<Master: RouterType, Detail: RouterType>: Router<UISplitViewController> {
+    public var master: Master
+    public var detail: Detail
+    private var delegate: UISplitViewControllerDelegate?
+    
+    public init(master: Master, detail: Detail, delegate: UISplitViewControllerDelegate? = nil) {
+        self.master = master
+        self.detail = detail
+        self.delegate = delegate
+        
+        let splitViewController = UISplitViewController()
+        splitViewController.viewControllers = [master.viewController, detail.viewController]
+        splitViewController.delegate = delegate
+        
+        super.init(viewController: splitViewController)
+    }
+}
--- Example/BestRouter/AppDelegate.swift
+++ Example/BestRouter/AppDelegate.swift
@@ -7,37 +7,74 @@
 //
 
 import UIKit
+import BestRouter
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // anchor for the router hierarchy
+    var router: Any!
 
-    var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let blueVC = UIViewController()
+        blueVC.view.backgroundColor = .blue
+        blueVC.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
+        
+        let redVC = UIViewController()
+        redVC.view.backgroundColor = .red
+        redVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        
+        let yellowVC = UIViewController()
+        yellowVC.view.backgroundColor = .yellow
+        
+        let vcRouter1 = Router(viewController: blueVC)
+        let vcRouter2 = Router(viewController: redVC)
+        let vcRouter3 = Router(viewController: yellowVC)
+        
+        let nav1 = NavigationRouter(root: vcRouter1)
+        let nav2 = NavigationRouter(root: vcRouter2)
+        let nav3 = NavigationRouter(root: vcRouter3)
+        
+        let tab = TabBarRouter(items: [nav1, nav2])
+        let split = SplitViewRouter(master: nav3, detail: tab)
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            // iPad
+            let router = WindowRouter(root: split, window: window)
+            router.launch()
+            self.router = router
+        } else {
+            // iPhone
+            let router = WindowRouter(root: tab, window: window)
+            router.launch()
+            self.router = router
+        }
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
