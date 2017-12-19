0e68fce2af3cdd53824a18482c0576dda2c6fb56
adam.cumiskey@gmail.com <adam.cumiskey@gmail.com>
2017-12-19 10:52:01 -0500

    Remove Pods/

--- Example/Pods/Target Support Files/BestRouter/BestRouter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BestRouter : NSObject
-@end
-@implementation PodsDummy_BestRouter
-@end
--- Example/Pods/Target Support Files/Pods-BestRouter_Example/Pods-BestRouter_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BestRouter_Example : NSObject
-@end
-@implementation PodsDummy_Pods_BestRouter_Example
-@end
